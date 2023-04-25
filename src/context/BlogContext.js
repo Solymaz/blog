33bbe9562e9858f7318f9abde9518e357f4e05b8
Solmaz <solmaz.mousavie@gmail.com>
2023-04-25 14:28:12 +0200

    moving data with Context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,5 +3,10 @@ import React from "react";
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-  return <BlogContext.Provider>{children}</BlogContext.Provider>;
+  const blogPosts = [{ title: "post 1" }, { title: "post 2" }];
+  return (
+    <BlogContext.Provider value={blogPosts}>{children}</BlogContext.Provider>
+  );
 };
+
+export default BlogContext;
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,6 +1,20 @@
-import React from "react";
-import { styleSheet, View, Text } from "react-native";
+import React, { useContext } from "react";
+import { View, Text, FlatList } from "react-native";
+import BlogContext from "../context/BlogContext";
 
 export default function IndexScreen() {
-  return <Text>Index screen</Text>;
+  const blogPosts = useContext(BlogContext);
+
+  return (
+    <View>
+      <Text>Index screen</Text>
+      <FlatList
+        data={blogPosts}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>;
+        }}
+        keyExtractor={(blogPost) => blogPost.title}
+      />
+    </View>
+  );
 }
