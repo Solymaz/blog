e3e735d483ad3d0076dc20a302a2cae46a822a97
Solmaz <solmaz.mousavie@gmail.com>
2023-04-26 20:01:24 +0200

    some styling

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,19 +1,41 @@
 import React, { useContext } from "react";
-import { View, Text, FlatList, Button } from "react-native";
+import { View, Text, FlatList, Button, StyleSheet } from "react-native";
 import Blog from "../context/Blog";
+import { Entypo } from "@expo/vector-icons";
 
 export default function IndexScreen() {
   const { data, addBlogPost } = useContext(Blog); // when we call useContext its gonna give us whatever we added in the value prop inside the provider
 
   return (
     <View>
-      <Text>Index screen</Text>
       <Button title="Add post" onPress={addBlogPost} />
       <FlatList
         data={data}
-        renderItem={({ item }) => <Text>{item.title}</Text>}
+        renderItem={({ item }) => (
+          <View style={styles.row}>
+            <Text style={styles.title}>{item.title}</Text>
+            <Entypo style={styles.icon} name="trash" />
+          </View>
+        )}
         keyExtractor={(blogPost) => blogPost.title}
       />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 15,
+    paddingHorizontal: 10,
+    borderTopWidth: 1,
+    borderColor: "gray",
+  },
+  title: {
+    fontSize: 18,
+  },
+  icon: {
+    fontSize: 24,
+  },
+});
