05b3ccc988b3c17ea89bdb0a5ebb1407f0e5fd50
Solmaz <solmaz.mousavie@gmail.com>
2023-05-01 23:01:46 +0200

    displaying add blog post form

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,10 +1,40 @@
-import React, { useContext } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useContext, useState } from "react";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 
 export default function CreateScreen({ navigation }) {
+  const [title, setTitle] = useState();
+  const [content, setConetnt] = useState();
+
   return (
     <View>
-      <Text>Create screen</Text>
+      <Text style={styles.label}>Enter Title:</Text>
+      <TextInput
+        value={title}
+        onChangeText={(text) => setTitle(text)}
+        style={styles.input}
+      />
+      <Text style={styles.label}>Enter Content:</Text>
+      <TextInput
+        style={styles.input}
+        value={content}
+        onChangeText={(text) => setConetnt(text)}
+      />
+      <Button title="Add Blog Post" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  input: {
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: "black",
+    marginHorizontal: 10,
+    padding: 5,
+    marginBottom: 10,
+  },
+  label: {
+    fontSize: 20,
+    margin: 10,
+  },
+});
